fix mel spec masking bug

data_generation.py
@@ -12,24 +12,24 @@ import matplotlib.pyplot as plt
 class LabelConvert:
     def __init__(self):
         label_map = '''
-        1 bar
-        2 skill
-        3 skull
-        4 bask
-        5 ill
-        6 Earl
-        7 Lee
-        8 steal
-        9 stale
-        10 span
-        11 in
-        12 least
-        13 an
-        14 eel
-        15 cusp
-        16 ale
-        17 Kerr
-        18 spin
+        0 bar
+        1 skill
+        2 skull
+        3 bask
+        4 ill
+        5 Earl
+        6 Lee
+        7 steal
+        8 stale
+        9 span
+        10 in
+        11 least
+        12 an
+        13 eel
+        14 cusp
+        15 ale
+        16 Kerr
+        17 spin
         '''
         # dictionaries to store label to word mapping
         self.syl_map = {}
@@ -50,33 +50,35 @@ class LabelConvert:
         return word_seq
 
 
-def converter(wav, sr, nmels=128, tweak=False, verbose=False):
+def converter(wav, sr, nmels=40, tweak=False, verbose=False):
     win_len = int(sr*0.025)
     hop = int(0.005*sr)
     tweaker = choice(['mask_time', 'mask_frequency', 'both_masks', 'noise'])
-    layers = [torchaudio.transforms.MelSpectrogram(sample_rate=sr, n_mels=nmels, win_length=win_len, n_fft=win_len,
-                                                   hop_length=hop)]
+    mel_net = torchaudio.transforms.MelSpectrogram(sample_rate=sr, n_mels=nmels, win_length=win_len, n_fft=win_len,
+                                                   hop_length=hop)
     if tweak:
         if tweaker != 'noise':
+            tweak_net = []
             if tweaker == 'mask_time':
-                layers.append(torchaudio.transforms.TimeMasking(time_mask_param=20))
+                tweak_net.append(torchaudio.transforms.TimeMasking(time_mask_param=20))
             elif tweaker == 'both_masks':
-                layers.append(torchaudio.transforms.FrequencyMasking(freq_mask_param=15))
-                layers.append(torchaudio.transforms.TimeMasking(time_mask_param=15))
+                tweak_net.append(torchaudio.transforms.FrequencyMasking(freq_mask_param=10))
+                tweak_net.append(torchaudio.transforms.TimeMasking(time_mask_param=15))
             else:
-                layers.append(torchaudio.transforms.FrequencyMasking(freq_mask_param=20))
+                tweak_net.append(torchaudio.transforms.FrequencyMasking(freq_mask_param=10))
+            tweak_net = nn.Sequential(*tweak_net)
         else:
             wav = wav + rand(wav.shape)*0.02
         if verbose:
             print(f'tweak: {tweaker}')
-    net = nn.Sequential(*layers)
-    converted = from_numpy(normalize(power_to_db(net(wav)[0])))
-    #converted = net(wav)[0]
+    converted = from_numpy(normalize(power_to_db(mel_net(wav)[0])))
+    if tweak and tweaker != 'noise':
+        converted = tweak_net(Tensor(converted))
     return converted
 
 
 def checkMel():
-    filename = "pilot/slow_sound_files/Lee_steal_5.wav"
+    filename = "pilot/slow_sound_files/bar_skill_12.wav"
     waveform, sampling_rate = torchaudio.load(filename)
     original = converter(waveform, sr=sampling_rate)
     tweaked = converter(waveform, sr=sampling_rate, tweak=True, verbose=True)

main.py
@@ -9,34 +9,33 @@ from torchvision.utils import make_grid
 
 # hyper-parameters and others
 params_args = {
-    'n_res_cnn': 5,
-    'n_rnn': 4,
+    'n_res_cnn': 3,
+    'n_rnn': 5,
     'rnn_dim': 512,
     'linear_dim': 512,
-    'n_class': 19,
-    'n_feats': 128,
+    'n_class': 18,
+    'n_feats': 40,
     'stride': 1,
-    'dropout': 0.2,
+    'dropout': 0.1,
     'n_convos': 32,
     'lr': 0.00001,
     'grad_clip': 400,
     'batch_size': 32,
-    'n_epochs': 200,
+    'n_epochs': 300,
     'h_rate': 0.1,
     'data_path': 'pilot',
-    'use_enctc': True
+    'use_enctc': True,
+    'blank': None
 }
 # to monitor training
-writer = tensorboard.SummaryWriter('runs/5_resnet_4_gru_lr_0.00001')
+writer = tensorboard.SummaryWriter('runs/no_blank_tiral_2_0010_rnn_5')
 
 net = Model(params_args['n_res_cnn'], params_args['n_rnn'], params_args['rnn_dim'], params_args['n_class'],
             params_args['n_feats'], params_args['linear_dim'], stride=1, dropout=params_args['dropout'],
             convo_channel=params_args['n_convos'])
 
-# load saved model
-#net.load_state_dict(torch.load('model_low_lr_wer87.pth'))
 
-train_data, val_data = loadData(params_args['data_path'])
+train_data, val_data = loadData(params_args['data_path'], train_tweak_ratio=0.3)
 
 
 # define optimiser
@@ -48,7 +47,7 @@ def train_enctc(train_iter):
     specs, labels, input_lens, label_lens = train_iter.next()
     labels = labels.flatten() # (batch)
     preds = net(specs).transpose(0, 1) # (time, batch, vocab +1)
-    H, cost = ctc_ent_cost(preds, labels, input_lens, label_lens)
+    H, cost = ctc_ent_cost(preds, labels, input_lens, label_lens, blank=params_args['blank'])
     cost_total = cost.data.sum()
     inf = float("inf")
     # in case of exploding gradient
@@ -77,8 +76,8 @@ def validation_enctc(val_loader):
         specs, labels, input_lens, label_lens = data
         preds = net(specs).transpose(0, 1)
         labels_flat = labels.flatten()
-        H, cost = ctc_ent_cost(preds, labels_flat, input_lens, label_lens)
-        wer, _ = utils.WER(preds.transpose(0, 1), labels)
+        H, cost = ctc_ent_cost(preds, labels_flat, input_lens, label_lens, blank=params_args['blank'])
+        wer, _ = utils.WER(preds.transpose(0, 1), labels, blank=params_args['blank'])
         wer_total += wer
         val_h_avg.add(H/len(labels_flat))
         val_loss_avg.add(cost/len(labels_flat))
@@ -121,7 +120,7 @@ def validation(loader, criterion):
         preds = F.log_softmax(origin_preds, dim=2).transpose(0, 1)
         loss = criterion(preds, labels, input_lens, label_lens)
         total_loss += loss.item()/len(input_lens)
-        wer, _ = utils.WER(origin_preds, labels)
+        wer, _ = utils.WER(origin_preds, labels, blank=params_args['blank'])
         total_wer += wer
     return total_loss/len(loader), total_wer/len(loader)
 
@@ -160,7 +159,7 @@ def main():
             print(f'epoch {epoch} wer: {val_wer}')
         writer.close()
     else:
-        criterion = torch.nn.CTCLoss(blank=0)
+        criterion = torch.nn.CTCLoss(blank=params_args['blank'])
         for epoch in range(params_args['n_epochs']):
             print(f'epoch: {epoch}')
             train_loss = train(train_loader, criterion)